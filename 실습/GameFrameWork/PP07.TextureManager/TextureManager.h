a2cf474120142ef0dd2c1f815611a204b6b8ac62
태휘 전 <20171270@vision.hoseo.edu>
2018-10-11 20:21:44 +0900

    07.TextureManager

---
+++ 실습/GameFrameWork/PP07.TextureManager/TextureManager.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<map>
+#include<SDL.h>
+#include<SDL_image.h>
+class TextureManager
+{
+private:
+	std::map<std::string, SDL_Texture*>m_textureMap;
+public:
+	bool load(std::string fileName, std::string id,
+		SDL_Renderer* pRenderer);
+
+	void draw(std::string id, int x, int y, int width, int height,
+		SDL_Renderer* pRenderer,
+		SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+	void drawFrame(std::string id, int x, int y, int width, int height,
+		int currentRow, int currentFrame,
+		SDL_Renderer* pRenderer,
+		SDL_RendererFlip flip = SDL_FLIP_NONE);
+};
